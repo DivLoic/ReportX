93224fde1c80856085fd840581034b2b0c4918b5
Loïc <ldivad@isep.fr>
2016-09-07 16:22:35 +0200

    add kibana and deploy ES

--- TP-Kafka/resources/rooit.sh
+++ TP-Kafka/resources/rooit.sh
@@ -15,37 +15,43 @@ vi .userrc
 
 #!/bin/bash
 
-#export PS1="\[\033[35;1m\]\]\w • \u $>\[\033[0m\]\]"
+export PS1="\[\033[35;1m\]\]\w • \u $>\[\033[0m\]\]"
 
-#export LIB_DIR=$HOME/lib
-#export APP_DIR=$HOME/products
-#export WORKDIR=$HOME/workspace
+export LIB_DIR=$HOME/lib
+export APP_DIR=$HOME/products
+export WORKDIR=$HOME/workspace
 
 # -- java
-#export JAVA_HOME=$APP_DIR/java
+export JAVA_HOME=$APP_DIR/java
 
 # -- scala
-#export SBT_HOME=$APP_DIR/sbt
-#export ACTIVATOR=$APP_DIR/activator
-#export SCALA_HOME=$APP_DIR/scala
+export SBT_HOME=$APP_DIR/sbt
+export ACTIVATOR=$APP_DIR/activator
+export SCALA_HOME=$APP_DIR/scala
 
 # -- elasticsearch
-#export ES_HOME=$APP_DIR/elasticsearch
+export ES_HOME=$APP_DIR/elasticsearch
+export KB_HOME=$APP_DIR/kiana
 
 # -- kafka
-#export KFK_HOME=$APP_DIR/kafka
+export KFK_HOME=$APP_DIR/kafka
+
+# -- Node 
+export NODE_HOME=$APP_DIR/node
 
 # -- Python
-#export CONDA_HOME=$APP_DIR/anaconda
-#alias jup='jupyter notebook --port 3333 --notebook-dir=$HOME/notebook'
+export CONDA_HOME=$APP_DIR/anaconda
+alias jup='jupyter notebook --port 3333 --notebook-dir=$HOME/notebook'
 
-#export PATH=$JAVA_HOME/bin:$PATH
-#export PATH=$CONDA_HOME/bin:$PATH
-#export PATH=$SBT_HOME/bin:$PATH
-#export PATH=$SCALA_HOME/bin:$PATH
-#export PATH=$ACTIVATOR/bin:$PATH
-#export PATH=$ES_HOME/bin:$PATH
-#export PATH=$KFK_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$CONDA_HOME/bin:$PATH
+export PATH=$SBT_HOME/bin:$PATH
+export PATH=$SCALA_HOME/bin:$PATH
+export PATH=$ACTIVATOR/bin:$PATH
+export PATH=$ES_HOME/bin:$PATH
+export PATH=$KFK_HOME/bin:$PATH
+export PATH=$NODE_HOME/bin:$PATH
+export PATH=$KB_HOME/bin:$PATH
 
 # we dl the sofware
 
@@ -54,6 +60,8 @@ vi .userrc
 #wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.0/elasticsearch-2.4.0.tar.gz
 #wget https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh
 #wget http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz
+#wget https://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x64.tar.xz
+#wget https://download.elastic.co/kibana/kibana/kibana-4.6.0-linux-x86_64.tar.gz
 
 #we add the links
 
@@ -62,3 +70,19 @@ ln -s ../lib/sbt/ sbt
 ln -s ../lib/scala-2.11.8/ scala 
 ln -s ../lib/jdk1.7.0_80/ java
 ln -s ../lib/anaconda-4.1.1/ anaconda
+ln -s ../lib/kibana-4.6.0-linux-x86_64/ kibana
+ln n -s ../lib/node-v4.5.0-linux-x64/ node
+
+
+# plugin elasticsearch 
+plugin install mobz/elasticsearch-head
+plugin install license
+plugin install marvel-agent
+
+# WARNING 
+#elasticsearch -Des.insecure.allow.root=true
+
+# elasticsearch config
+cluster.name: kafka-lab
+network.host: 0.0.0.0
+discovery.zen.ping.unicast.hosts: ["ip1", "ip2", "ip3"]
